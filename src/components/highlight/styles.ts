7d1a6921fd28fb7cfad79a635f2858c6c2e7d0b6
Wagner Coche <dev003.mobile@gmail.com>
2024-09-15 14:06:13 +0100

    🐳 chore: highlight component

---
+++ src/components/highlight/styles.ts
@@ -0,0 +1,20 @@
+import { styled } from "styled-components/native"
+
+export const Container = styled.View({
+   width: "100%",
+   alignItems: "center",
+   paddingTop: 30,
+   justifyContent: "center",
+})
+
+export const Title = styled.Text(({ theme }) => ({
+   color: theme.COLORS.WHITE,
+   fontFamily: theme.FONT_FAMILY.BOLD,
+   fontSize: theme.FONT_SIZE.XL
+}))
+
+export const Subtitle = styled.Text(({ theme }) => ({
+   color: theme.COLORS.GRAY_300,
+   fontFamily: theme.FONT_FAMILY.REGULAR,
+   fontSize: theme.FONT_SIZE.MD
+}))
--- src/screens/group-screen/index.tsx
+++ src/screens/group-screen/index.tsx
@@ -1,10 +1,13 @@
-import { Header } from "@components/header"
 import { Container } from "./styles"
+import { Header } from "@components/header"
+import { Highlight } from "@components/highlight"
+
 
 export function GroupScreen() {
    return (
       <Container>
          <Header />
+         <Highlight title="Turmas" subtitle="Jogue com a sua turma"/>
       </Container>
    )
 }
